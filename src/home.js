ceb09a6e11db3f480ef087247562d2f37fb2b8b7
Scott Alton <scottaltonmusic@gmail.com>
2021-05-03 14:13:19 -0400

    Basic styles/layout added for homepage.

--- src/home.js
+++ src/home.js
@@ -28,18 +28,25 @@ const showHomepage = () => {
   // Generate hero section
   let heroContainer = document.createElement("div");
   heroContainer.classList.add("hero");
-  let heroImage = document.createElement("img");
-  heroImage.src = "../images/fryway-33.jpg";
   let leadParagraph = document.createElement("p");
+  leadParagraph.classList.add("lead");
   leadParagraph.innerText =
     "More than your average chip truck, we serve authentic Quebec-style poutine, and a ton of other classic comfort foods.";
+  let menuButton = document.createElement("button");
+  menuButton.classList.add("btn");
+  menuButton.innerText = "View Menu";
+
   let descriptionParagraph = document.createElement("p");
   descriptionParagraph.innerText =
     "For the last 7 years, Scott and his team have been cooking up a storm in the small city of Bath, Ontario. Tired of the limited options between Toronto and Ottawa, FryWay33 offers premium quality-comfort food. Gradually becoming a classic family stop, we are serving up the best fish and chips, burgers, and poutine to be found east of Toronto.";
-  heroContainer.appendChild(heroImage);
+
   heroContainer.appendChild(leadParagraph);
-  heroContainer.appendChild(descriptionParagraph);
+  heroContainer.appendChild(menuButton);
   contentContainer.appendChild(heroContainer);
+
+  let footer = document.createElement("footer");
+  footer.innerText = "Footer";
+  contentContainer.appendChild(footer);
 };
 
 module.exports = { showHomepage };
