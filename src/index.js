e8415c041a9f87fd3d8e7e8c229c3658f7cf90ca
Scott Alton <scottaltonmusic@gmail.com>
2021-05-04 11:50:38 -0400

    Expanded router.

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ import { showAbout } from "./about";
 import { showContact } from "./contact";
 import { showFooter } from "./footer";
 
-let page = "home";
+let activeLink = "";
 
 showNavbar();
 const homeLink = document.querySelector('[data-link="home"]');
@@ -13,22 +13,44 @@ const menuLink = document.querySelector('[data-link="menu"]');
 const aboutLink = document.querySelector('[data-link="about"]');
 const contactLink = document.querySelector('[data-link="contact"]');
 
-switch (page) {
-  case "home":
-    homeLink.style.backgroundColor = "red";
-    showHomepage();
-    break;
-  case "menu":
-    menuLink.style.backgroundColor = "red";
-    showMenu();
-    break;
-  case "about":
-    aboutLink.style.backgroundColor = "red";
-    showAbout();
-    break;
-  case "contact":
-    contactLink.style.backgroundColor = "red";
-    showContact();
-    break;
+let links = [homeLink, menuLink, aboutLink, contactLink];
+
+function route() {
+  switch (activeLink) {
+    case "home":
+    case "":
+      showHomepage();
+      break;
+    case "menu":
+      showMenu();
+      break;
+    case "about":
+      showAbout();
+      break;
+    case "contact":
+      showContact();
+      break;
+  }
 }
+
+route();
 showFooter();
+addLinkEvents();
+function addLinkEvents() {
+  // Add click events to change active links
+  links.forEach((link) => {
+    link.addEventListener("click", (e) => {
+      links.forEach((link) => {
+        link.style.backgroundColor = "#fff";
+      });
+      alert("clicked" + e.target.getAttribute("data-link"));
+      activeLink = e.target.getAttribute("data-link");
+      alert("ActiveLink set to " + activeLink);
+      document.querySelector(
+        `[data-link="${e.target.getAttribute("data-link")}"]`
+      ).style.backgroundColor = "red";
+    });
+
+    route();
+  });
+}
