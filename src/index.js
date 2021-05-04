dd773da6b8cbb26a5bd3325ac23d039a9f208e53
Scott Alton <scottaltonmusic@gmail.com>
2021-05-04 10:43:28 -0400

    Highlighting active link added and piecing front-end router together.

--- src/index.js
+++ src/index.js
@@ -8,17 +8,26 @@ import { showFooter } from "./footer";
 let page = "home";
 
 showNavbar();
+const homeLink = document.querySelector('[data-link="home"]');
+const menuLink = document.querySelector('[data-link="menu"]');
+const aboutLink = document.querySelector('[data-link="about"]');
+const contactLink = document.querySelector('[data-link="contact"]');
+
 switch (page) {
   case "home":
+    homeLink.style.backgroundColor = "red";
     showHomepage();
     break;
   case "menu":
+    menuLink.style.backgroundColor = "red";
     showMenu();
     break;
   case "about":
+    aboutLink.style.backgroundColor = "red";
     showAbout();
     break;
   case "contact":
+    contactLink.style.backgroundColor = "red";
     showContact();
     break;
 }
--- src/navbar.js
+++ src/navbar.js
@@ -8,12 +8,16 @@ const showNavbar = () => {
   navItems.classList.add("nav-links");
   let homeLink = document.createElement("li");
   homeLink.innerText = "Home";
+  homeLink.setAttribute("data-link", "home");
   let menuLink = document.createElement("li");
   menuLink.innerText = "Menu";
+  menuLink.setAttribute("data-link", "menu");
   let aboutLink = document.createElement("li");
   aboutLink.innerText = "About";
+  aboutLink.setAttribute("data-link", "about");
   let contactLink = document.createElement("li");
   contactLink.innerText = "Contact";
+  contactLink.setAttribute("data-link", "contact");
 
   navItems.appendChild(homeLink);
   navItems.appendChild(menuLink);
